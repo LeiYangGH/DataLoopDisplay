ebb6401c4a0b38f19229da7b9f442f4775514599
LeiYangGH <675686066@qq.com>
2018-07-01 08:50:34 +0800

    add views folder and can run mainwindow

--- DataLoopDisplay/Views/MainWindow.xaml.cs
+++ DataLoopDisplay/Views/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace DataLoopDisplay
+namespace DataLoopDisplay.Views
 {
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
